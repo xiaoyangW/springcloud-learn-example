a60e5bd4b7bfbd65ee9f54d1f6c9ce3d5475bd0d
xiaoyang <1483863489@qq.com>
2017-09-12 22:15:38 +0800

    添加eureka-服务发现项目

---
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/Application.java
@@ -0,0 +1,17 @@
+package com.itmuch.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka application
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application{
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class,args);
+    }
+}
