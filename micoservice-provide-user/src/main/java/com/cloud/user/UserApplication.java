1c63b9f1b6b5c2f9d4f97c14d978e5e87014f737
xiaoyang <1483863489@qq.com>
2017-09-14 21:09:55 +0800

    添加eureka-服务发现项目

---
+++ micoservice-provide-user/src/main/java/com/cloud/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableEurekaClient
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
