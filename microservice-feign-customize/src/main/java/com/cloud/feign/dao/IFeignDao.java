5bf74b52bb74d56eacd9aaa41c0ca55d3fd3956c
xiaoyang <1483863489@qq.com>
2017-11-13 13:29:06 +0800

    修改

--- microservice-feign-customize/src/main/java/com/cloud/feign/dao/IFeignDao.java
+++ microservice-feign-customize/src/main/java/com/cloud/feign/dao/IFeignDao.java
@@ -4,9 +4,6 @@ import com.cloud.feign.dto.ResponseDto;
 import com.cloud.feign.feign.TestConfiguration;
 import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "micoserice-user",configuration = TestConfiguration.class)
 public interface IFeignDao {
