0966e7aef78c9d6dfccd22feec5e6b7d8ad27a3b
xiaoyang <1483863489@qq.com>
2017-12-26 10:16:19 +0800

    hystrix-dashboard-turbine

---
+++ microservice-hystrixdashboard-turbine/src/main/java/com/cloud/ribbon/HystrixTurbineApplication.java
@@ -0,0 +1,28 @@
+package com.cloud.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+@EnableTurbine
+public class HystrixTurbineApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixTurbineApplication.class, args);
+	}
+}
