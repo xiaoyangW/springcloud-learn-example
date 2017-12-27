67a3ce09c56b77cfb1b20540ffdb8d173bcb3410
xiaoyang <1483863489@qq.com>
2017-12-27 16:55:34 +0800

    zuul,api-gateway

---
+++ microservice-gateway-zuul/src/main/java/com/cloud/zuul/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.cloud.zuul.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
