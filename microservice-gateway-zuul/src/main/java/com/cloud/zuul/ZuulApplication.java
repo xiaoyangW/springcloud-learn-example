87b45f535d6c6e995330e089c85a328f3086b3ab
xiaoyang <1483863489@qq.com>
2018-01-12 13:42:52 +0800

    添加使用正则表达式路由方式

--- microservice-gateway-zuul/src/main/java/com/cloud/zuul/ZuulApplication.java
+++ microservice-gateway-zuul/src/main/java/com/cloud/zuul/ZuulApplication.java
@@ -2,8 +2,9 @@ package com.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,5 +12,18 @@ public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	/**
+	 * 使用正则表达式方式路由，
+	 * 添加该方式后,在原有的服务中，appname-v1,
+	 * 如 micoserice-user-v1
+	 * @return
+	 */
+	@Bean
+	public PatternServiceRouteMapper serviceRouteMapper(){
+		return new PatternServiceRouteMapper(
+				"(?<name>^.+)-(?<version>v.+$)",
+				"${version}/${name}");
 	}
 }
