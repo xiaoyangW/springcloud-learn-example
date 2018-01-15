46c4e4801d8e76571c83cc3e1c51dc667b38a99b
xiaoyang <1483863489@qq.com>
2018-01-15 17:50:30 +0800

    在zuul中添加hystrix回退

---
+++ microservice-gateway-zuul/src/main/java/com/cloud/zuul/config/MyFallbackProvider.java
@@ -0,0 +1,64 @@
+package com.cloud.zuul.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * @author  wxy
+ *  zuul中使用hystrix的回退
+ */
+@Component
+public class MyFallbackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "micoserice-user";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "ok";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("服务不可用稍后再试".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mediaType = new MediaType("application","json", Charset.forName("UTF-8"));
+                headers.setContentType(mediaType);
+                return headers;
+            }
+        };
+    }
+}
