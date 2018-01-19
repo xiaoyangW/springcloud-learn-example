32bb7b81925e915dcf4d039268d76473aa7f6579
xiaoyang <1483863489@qq.com>
2018-01-19 17:24:07 +0800

    添加config-client

---
+++ microservice-config-client/src/main/java/com/cloud/ConfigClinetApplication.java
@@ -0,0 +1,14 @@
+package com.cloud;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@Configurable
+@EnableAutoConfiguration
+public class ConfigClinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClinetApplication.class, args);
+	}
+}
