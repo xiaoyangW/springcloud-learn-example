ddd7e4621808f8895b22d9c99d7f4c7e99ccba22
xiaoyang <1483863489@qq.com>
2018-01-25 14:59:14 +0800

    t

--- microservice-sleuth-zipkin-server/src/main/java/com/cloud/zipkin/ZipKinServerApplication.java
+++ microservice-sleuth-zipkin-server/src/main/java/com/cloud/zipkin/ZipKinServerApplication.java
@@ -2,13 +2,13 @@ package com.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
 
 /**
  * @author wxy
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableZipkinServer
 public class ZipKinServerApplication {
 
 	public static void main(String[] args) {
