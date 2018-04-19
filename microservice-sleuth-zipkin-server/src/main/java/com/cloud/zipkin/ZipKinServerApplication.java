dcf11a25a76c113250cd676764954443f95868c7
xiaoyang <1483863489@qq.com>
2018-04-19 21:48:15 +0800

    添加zipkin配置

--- microservice-sleuth-zipkin-server/src/main/java/com/cloud/zipkin/ZipKinServerApplication.java
+++ microservice-sleuth-zipkin-server/src/main/java/com/cloud/zipkin/ZipKinServerApplication.java
@@ -2,6 +2,7 @@ package com.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 /**
@@ -9,6 +10,7 @@ import zipkin.server.EnableZipkinServer;
  */
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class ZipKinServerApplication {
 
 	public static void main(String[] args) {
